2efd29b4a54fa316cf58d84bd7246275a42aae8c
ImranHossain5722 <imd572258@gmail.com>
2022-08-15 17:49:01 +0600

    firebase deploy and wirte redme file

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWr6hH1xVnFgj4B6x4KsUJhj0DdqwtpkY",
+  authDomain: "igclone-82335.firebaseapp.com",
+  projectId: "igclone-82335",
+  storageBucket: "igclone-82335.appspot.com",
+  messagingSenderId: "246354702416",
+  appId: "1:246354702416:web:90904bca8bfe13f0096155"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
